52fc57011568c823af306d33f07649d805e4849d
ronald <ronaldjunior2@hotmail.com>
2021-05-11 16:36:58 -0300

    preparando review

--- src/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <div class="header">
-      <div class="titulo">FoodCamp</div>
-      <div class="subtitulo">Sua comida em 6 minutos</div>
-    </div>
-  );
-}
--- src/Item.js
+++
@@ -1,9 +0,0 @@
-export default function Item(item) {
-    return (
-      <li class="sobremesa">
-        <div class="nome">{item.nome}</div>
-        <div class="quantidade">{item.quantidade}</div>
-        <div class="preco">{item.preco}</div>
-      </li>
-    );
-  }
